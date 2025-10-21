cd990c61f9536e88cfd21a6e861e9dfe17b80fa3
nickjohn63 <nick.birchall@btinternet.com>
2025-10-21 22:11:31 +0100

    Update cache name to version 7

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Offline cache for GitHub Pages
-const CACHE_NAME = 'five-tab-todo-cache-v6'; // bumped
+const CACHE_NAME = 'five-tab-todo-cache-v7'; // bumping
 const ASSETS = [
   './',
   './index.html',
